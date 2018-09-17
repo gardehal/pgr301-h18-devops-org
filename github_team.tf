21629f301138117ecb678913ed53bfd07c3133f7
Alexander <garale16@student.westerdals.no>
2018-09-17 11:24:27 +0200

    repo delete + member

--- github_team.tf
+++ github_team.tf
@@ -1,4 +1,12 @@
-resource "github_team" "theclass" {
+resource "github_team" "theteam" {
   name        = "pgr301class"
-  description = "The students and teacher of the class"
+  description = "The students of the class"
 }
+
+resource "github_team_membership" "grdall" {
+  team_id  = "${github_team.theclass.id}"
+  username = "grdall"
+  role     = "member"
+}
+
+
