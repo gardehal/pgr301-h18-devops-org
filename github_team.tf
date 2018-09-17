04faa9d4b5a71f06ca15195311ad5bd7a50da7ef
Alexander <garale16@student.westerdals.no>
2018-09-17 10:59:13 +0200

    terraform success

--- github_team.tf
+++ github_team.tf
@@ -2,35 +2,3 @@ resource "github_team" "theclass" {
   name        = "pgr301class"
   description = "The students and teacher of the class"
 }
-
-resource "github_team_membership" "glennbech" {
-  team_id  = "${github_team.theclass.id}"
-  username = "glennbech"
-  role     = "member"
-}
-
-resource "github_team_membership" "larstobi" {
-  team_id  = "${github_team.theclass.id}"
-  username = "larstobi"
-  role     = "member"
-}
-
-resource "github_team_membership" "minimarker" {
-  team_id  = "${github_team.theclass.id}"
-  username = "minimarker"
-  role     = "member"
-}
-
-
-resource "github_team_membership" "gabtho15" {
-  team_id  = "${github_team.theclass.id}"
-  username = "gabtho15"
-  role     = "member"
-}
-
-resource "github_team_membership" "husmas15" {
-  team_id  = "${github_team.theclass.id}"
-  username = "husmas15"
-  role     = "member"
-}
-
