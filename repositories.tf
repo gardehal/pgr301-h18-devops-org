094e822ddf74182957bc68d4ce991587f21f48b7
Alexander <garale16@student.westerdals.no>
2018-10-08 12:02:48 +0200

    repo setup up to l7

--- repositories.tf
+++ repositories.tf
@@ -11,6 +11,27 @@
 resource "github_repository" "l4"
 {
   name = "l4"
-  description = "Lesson 4: Terraform"
+  description = "Lesson 4: Forking, Terraform"
+  private = false
+}
+
+resource "github_repository" "l5"
+{
+  name = "l5"
+  description = "Lesson 5: Heroku, Concourse"
+  private = false
+}
+
+resource "github_repository" "l6"
+{
+  name = "l6"
+  description = "Lesson 6: Heroku, Secrets, Pipelines"
+  private = false
+}
+
+resource "github_repository" "l7"
+{
+  name = "l7"
+  description = "Lesson 7: Heroku, Pipelines, Deploy Cycle"
   private = false
 }
